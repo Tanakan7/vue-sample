d51520c5b7a1353a711bc361ab94dc632843f8b8
Tanakan7 <ywff34tk@gmail.com>
2019-07-29 22:35:27 +0900

    counter1作成

--- src/store.js
+++ src/store.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import counter from './store/modules/counter'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  strict: process.env.NODE_ENV !== 'production',
+  modules: {
+    counter
+  },
   state: {
 
   },
